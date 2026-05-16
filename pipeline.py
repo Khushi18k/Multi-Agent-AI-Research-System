89362d9c9cc46ff9d8ce87278e4a2b6a4f11c11b
khushi khandelwal <khushikhandelwal918@gmail.com>
2026-05-16 23:06:35 +0530

    first commit

---
+++ pipeline.py
@@ -0,0 +1,72 @@
+from agents import build_search_agent, build_reader_agent, writer_chain, critic_chain
+
+def run_research_pipeline(topic:str)->dict:
+
+      state={}
+
+      #search agent working
+      print("\n"+" ="*50)
+      print("step 1 - Search agent is worki...")
+      print("="*50)
+
+      search_agent=build_search_agent()
+      search_result=search_agent.invoke({
+            "messages":[("user",f"Find recent,reliable and detailed information about: {topic}")]
+            })
+      
+      state['search_results']=search_result["messages"][-1].content\
+      
+      print("\n Search Results:",state['search_results'])
+
+#Step-2 - Reader agent 
+      print("\n"+" ="*50)
+      print("step 2 - Reader agent is scraping top resources...")
+      print("="*50)
+
+      reader_agent=build_reader_agent()
+      reader_result=reader_agent.invoke({
+            "messages" : [("user",
+                f"Based on the following search results about '{topic}',"
+                f"pick the most relevant URLs and scrape it for deeper content.\n\n"
+                f"Search Results:\n{state['search_results'][:800]}"           
+                           )]
+      })
+
+      state['scraped_content']=reader_result["messages"][-1].content
+
+      print("\n Scraped Content\n:",state['scraped_content'])
+
+#Step-3 - Writer chain  
+      print("\n"+" ="*50)
+      print("step 3 - Writer drafting the research report...")
+      print("="*50)
+    
+      research_combined=(
+            f"SEARCH RESULTS:\n{state['search_results']}\n\n"
+            f"DETAILED SCRAPED CONTENT:\n{state['scraped_content']}"
+      )
+
+      state["report"]=writer_chain.invoke({
+            "topic": topic,
+            "research": research_combined
+      })
+
+      print("\n Final Report:\n",state['report'])
+
+      #critic report
+      print("\n"+" ="*50)
+      print("step 4 - Critic evaluating the report...")
+      print("="*50) 
+
+      state["feedback"]=critic_chain.invoke({
+            "report": state['report']
+      })
+
+      print("\n Critic Feedback:\n",state['feedback'])
+
+      return state
+
+if __name__=="__main__":      
+      topic=input("Enter a research topic: ")
+      run_research_pipeline(topic)
+      
